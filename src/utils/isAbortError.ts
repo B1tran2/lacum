3213846cd8db3e5b509dda6bc1bd0b520b96b7d5
B1tran2 <beltran.gv1@gmail.com>
2026-02-21 12:01:08 +0100

    Make isAbortError safe without DOMException

--- src/utils/isAbortError.ts
+++ src/utils/isAbortError.ts
@@ -1,6 +1,8 @@
 export function isAbortError(err: unknown): boolean {
-  return (
-    (err instanceof Error || err instanceof DOMException) &&
-    err.name === 'AbortError'
-  );
+  const isErrorAbort = err instanceof Error && err.name === 'AbortError';
+  const hasDOMException = typeof DOMException !== 'undefined';
+  const isDomAbort =
+    hasDOMException && err instanceof DOMException && err.name === 'AbortError';
+
+  return isErrorAbort || isDomAbort;
 }
